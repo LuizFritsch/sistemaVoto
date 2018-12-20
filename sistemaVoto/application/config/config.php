892d9ee00653fff36b73277895aa4945050991e3
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2018-12-20 12:03:45 -0200

    Update config.php

--- sistemaVoto/application/config/config.php
+++ sistemaVoto/application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://10.2.242.194/RPVI-GP4/sistemaVoto/';
+$config['base_url'] = 'http://localhost/sistemaVoto/sistemaVoto/';
 
 /*
 |--------------------------------------------------------------------------
